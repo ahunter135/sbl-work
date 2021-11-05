138d0d2e6528f5ddb221ecf193043c6fb8afc9dd
ahunter135 <austin.hunter@payzer.com>
2021-11-05 12:12:41 -0500

    add cap

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.achu.worksbl',
+  appName: 'work-sbl',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
